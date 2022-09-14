f1f0d66778443a6d6ab367a4aaebeaf8a9e15ffe
Bojan Vrhovnik <bojan@vrhovnik.net>
2022-09-14 13:15:12 +0200

    initial commit for the demos

---
+++ src/ISDemosSLN/IS.REST/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Calling REST to get back the information!");
+
+using var client = new  HttpClient();
+var clusterApiUrl = Environment.GetEnvironmentVariable("APIMASTERURL") ?? "https://localhost:8090";
+client.BaseAddress = new Uri(clusterApiUrl, UriKind.RelativeOrAbsolute);
